f03eabaf2bf5fa24fe9945a012678d731baec0a8
Veritas <ntantardini@gmail.com>
2025-11-30 16:11:15 -0800

    deployment

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+
+  // Enable static exports for VPS deployment
+  output: 'export',
+
+  // Ensure images work in static export (disable Next.js image optimization)
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
