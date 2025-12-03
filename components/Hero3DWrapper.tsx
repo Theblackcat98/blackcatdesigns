44ee08dae8e99a03724a917f868a4722cc32613d
GitHub Action <action@github.com>
2025-12-03 00:15:59 +0000

    Deploy static site - 2025-12-03 00:15:59

--- components/Hero3DWrapper.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import dynamic from 'next/dynamic'
-
-const Hero3D = dynamic(() => import('./Hero3D'), { ssr: false })
-
-export default function Hero3DWrapper() {
-    return <Hero3D />
-}
